bc27cd45ab2226a7bd32df4e904f34201b72e7a9
RuteBettencourt <101585150+RuteBettencourt@users.noreply.github.com>
2022-03-15 16:54:59 +0000

    Update dynCon_v2.m

--- dynCon_v2.m
+++ dynCon_v2.m
@@ -1,7 +1,7 @@
 %% 
 clear all, close all, clc
-
-data = fullfile('DATAPOOL', 'VPHYSTERESIS', 'BOLD_timecourses', 'ROIs_BOLD_timecourse.mat');
+path = strip(fullfile(' ','DATAPOOL', 'VPHYSTERESIS', 'BOLD_timecourses'));
+data = fullfile(path, 'ROIs_BOLD_timecourse.mat');
 load(data);
 
 subjects = 1:25;
@@ -9,6 +9,7 @@ effect_block = 5;
 
 metrics = struct();
 ROIs = fieldnames(BOLD_denoised_timecourse);
+ROIs_clean = strrep(ROIs,'+','');
 nROIs = length(ROIs);
 
 runs = fieldnames(BOLD_denoised_timecourse.SS_hMT_bilateral);
@@ -20,8 +21,8 @@ for run = nruns
         for seed = 1:nROIs
             for rr= 1:nROIs
                 if seed ~= rr
-                    rhos1 = []; 
-                    pvals1 = []; 
+                    rhos1 = []; rhos2 = [];
+                    pvals1 = []; pvals2 = [];
                     window = 1:effect_block;
                     if run==1
                         while window(end)<1065
@@ -33,6 +34,14 @@ for run = nruns
                             pval1 = diag(pval1);
                             rhos1 = [rhos1 rho1];
                             pvals1 = [pvals1 pval1];
+                            
+                            %Spearman correlation and p-value
+                            [rho2, pval2] = corr(BOLD_denoised_timecourse.(ROIs{seed}).(runs{run})(window,:),...
+                                BOLD_denoised_timecourse.(ROIs{rr}).(runs{run})(window,:), 'Type', 'Spearman');
+                            rho2 = diag(rho2);
+                            pval2 = diag(pval2);
+                            rhos2 = [rhos2 rho2];
+                            pvals2 = [pvals2 pval2];
 
                             window = window+1;
 
@@ -49,6 +58,14 @@ for run = nruns
                             pval1 = diag(pval1);
                             rhos1 = [rhos1 rho1];
                             pvals1 = [pvals1 pval1];
+                            
+                            %Spearman correlation and p-value
+                            [rho2, pval2] = corr(BOLD_denoised_timecourse.(ROIs{seed}).(runs{run})(window,:),...
+                                BOLD_denoised_timecourse.(ROIs{rr}).(runs{run})(window,:), 'Type', 'Spearman');
+                            rho2 = diag(rho2);
+                            pval2 = diag(pval2);
+                            rhos2 = [rhos2 rho2];
+                            pvals2 = [pvals2 pval2];
 
                             window = window+1;
 
@@ -57,10 +74,12 @@ for run = nruns
 
                     %%%%%%%%%%%%%%%%%%%%%
                     rhos1 = rhos1'; pvals1 = pvals1';
+                    rhos2 = rhos2'; pvals2 = pvals2';
 
-
-                    metrics.(ROIs{seed}).(ROIs{rr}).corrcoef.(runs{run}) = rhos1;
-                    metrics.(ROIs{seed}).(ROIs{rr}).corrpval.(runs{run}) = pvals1;
+                    metrics.(ROIs{seed}).(ROIs{rr}).corrcoefPearson.(runs{run}) = rhos1;
+                    metrics.(ROIs{seed}).(ROIs{rr}).corrpvalPearson.(runs{run}) = pvals1;
+                    metrics.(ROIs{seed}).(ROIs{rr}).corrcoefSpearman.(runs{run}) = rhos2;
+                    metrics.(ROIs{seed}).(ROIs{rr}).corrpvalSpearman.(runs{run}) = pvals2;
 
                 end
             end
@@ -69,44 +88,45 @@ for run = nruns
 end
 
 %%
-save('dynCon_metrics.mat', 'metrics')
+save(fullfile(path,'dynCon_metrics.mat'), 'metrics')
 
 %%
 
 % --------------Uncomment if running by sections--------------------------
-clear all, close all, clc
-datacon = fullfile('DATAPOOL', 'VPHYSTERESIS', 'BOLD_timecourses', 'dynCon_metrics.mat');
-data = fullfile('DATAPOOL', 'VPHYSTERESIS', 'BOLD_timecourses', 'ROIs_BOLD_timecourse.mat');
-load(data); load(datacon);
-
-ROIs = fieldnames(BOLD_denoised_timecourse);
-nROIs = length(ROIs);
-
-runs = fieldnames(BOLD_denoised_timecourse.SS_hMT_bilateral);
-nruns =1:length(runs); 
-
-for run = nruns
-    for seed = 1:nROIs
-        for rr= 1:nROIs
-            if seed ~= rr
-
-                %Correlation coefficients mean
-                metrics.(ROIs{seed}).(ROIs{rr}).meancorrcoef.(runs{run}) = mean(metrics.(ROIs{seed}).(ROIs{rr}).corrcoef.(runs{run}));
-               
-                %Correlation p-values mean
-                metrics.(ROIs{seed}).(ROIs{rr}).meancorrpval.(runs{run}) = mean(metrics.(ROIs{seed}).(ROIs{rr}).corrpval.(runs{run}));
-
-                %Correlation coefficients standard deviation
-                metrics.(ROIs{seed}).(ROIs{rr}).stdcorrcoef.(runs{run}) = std(metrics.(ROIs{seed}).(ROIs{rr}).corrcoef.(runs{run}));
-
-                %Correlation p-values standard deviation
-                metrics.(ROIs{seed}).(ROIs{rr}).stdcorrpval.(runs{run}) = std(metrics.(ROIs{seed}).(ROIs{rr}).corrpval.(runs{run}));
-                
-                %Fisher-transformed correlation coefficients
-                metrics.(ROIs{seed}).(ROIs{rr}).fisher.(runs{run}) = atanh(metrics.(ROIs{seed}).(ROIs{rr}).corrcoef.(runs{run}));
-            end
-        end
-    end
-end
-
-save('dynCon_metrics.mat', 'metrics')
+% clear all, close all, clc
+% path = strip(fullfile(' ','DATAPOOL', 'VPHYSTERESIS', 'BOLD_timecourses'));
+% datacon = fullfile(path, 'dynCon_metrics.mat');
+% data = fullfile(path, 'ROIs_BOLD_timecourse.mat');
+% load(data); load(datacon);
+% 
+% ROIs = fieldnames(BOLD_denoised_timecourse);
+% nROIs = length(ROIs);
+% 
+% runs = fieldnames(BOLD_denoised_timecourse.SS_hMT_bilateral);
+% nruns =1:length(runs); 
+% 
+% for run = nruns
+%     for seed = 1:nROIs
+%         for rr= 1:nROIs
+%             if seed ~= rr
+% 
+%                 %Correlation coefficients mean
+%                 metrics.(ROIs{seed}).(ROIs{rr}).meancorrcoef.(runs{run}) = mean(metrics.(ROIs{seed}).(ROIs{rr}).corrcoef.(runs{run}));
+%                
+%                 %Correlation p-values mean
+%                 metrics.(ROIs{seed}).(ROIs{rr}).meancorrpval.(runs{run}) = mean(metrics.(ROIs{seed}).(ROIs{rr}).corrpval.(runs{run}));
+% 
+%                 %Correlation coefficients standard deviation
+%                 metrics.(ROIs{seed}).(ROIs{rr}).stdcorrcoef.(runs{run}) = std(metrics.(ROIs{seed}).(ROIs{rr}).corrcoef.(runs{run}));
+% 
+%                 %Correlation p-values standard deviation
+%                 metrics.(ROIs{seed}).(ROIs{rr}).stdcorrpval.(runs{run}) = std(metrics.(ROIs{seed}).(ROIs{rr}).corrpval.(runs{run}));
+%                 
+%                 %Fisher-transformed correlation coefficients
+%                 metrics.(ROIs{seed}).(ROIs{rr}).fisher.(runs{run}) = atanh(metrics.(ROIs{seed}).(ROIs{rr}).corrcoef.(runs{run}));
+%             end
+%         end
+%     end
+% end
+% 
+% save(fullfile(path,'dynCon_metrics.mat'), 'metrics')
