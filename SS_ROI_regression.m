5d85c71992c85ba357a68375e0122a31a427f6c6
RuteBettencourt <101585150+RuteBettencourt@users.noreply.github.com>
2022-08-08 10:26:12 +0000

    Update SS_ROI_regression.m

--- SS_ROI_regression.m
+++ SS_ROI_regression.m
@@ -2,7 +2,7 @@
 clear all, close all, clc
 
 %%
-ROI = {'IPS', 'SPL', 'V3A', 'hMT', 'FEF', 'insula'};
+ROI = {'IPS', 'SPL', 'V3A', 'hMT', 'FEF', 'INSpeak'}; %'insula' 'INScorrected'
 runs = {'run1' 'run2' 'run3' 'run4'};
 %%
 %% Conditions definition
@@ -34,7 +34,7 @@ for run = 1:4
     
     for trial = 1:4
         comppatt(volumes.(runs{run}).CompPatt(trial,1):volumes.(runs{run}).CompPatt(trial,1)+nvolsmov) = 1;
-        pattcomp(volumes.(runs{run}).PattComp(trial,1):volumes.(runs{run}).CompPatt(trial,1)+nvolsmov) = 1;
+        pattcomp(volumes.(runs{run}).PattComp(trial,1):volumes.(runs{run}).PattComp(trial,1)+nvolsmov) = 1;
     end
     vols.(runs{run}).CompPatt = comppatt;
     vols.(runs{run}).PattComp = pattcomp;
@@ -50,17 +50,23 @@ hrf = spm_hrf(1.5);
 
 for run = 1:4
     
-    CompPatt_effect = conv(vols.(runs{run}).CompPatt,hrf, 'same');
-    PattComp_effect = conv(vols.(runs{run}).PattComp,hrf, 'same');
-    Discard_effect = conv(vols.(runs{run}).Discard,hrf, 'same');
-    Static_effect = conv(vols.(runs{run}).Static,hrf, 'same');
-    MAE_effect = conv(vols.(runs{run}).MAE,hrf, 'same');
+%     CompPatt_effect = conv(vols.(runs{run}).CompPatt,hrf, 'same');
+%     PattComp_effect = conv(vols.(runs{run}).PattComp,hrf, 'same');
+%     Discard_effect = conv(vols.(runs{run}).Discard,hrf, 'same');
+%     Static_effect = conv(vols.(runs{run}).Static,hrf, 'same');
+%     MAE_effect = conv(vols.(runs{run}).MAE,hrf, 'same');
+
+    CompPatt_effect = conv(vols.(runs{run}).CompPatt,hrf);
+    PattComp_effect = conv(vols.(runs{run}).PattComp,hrf);
+    Discard_effect = conv(vols.(runs{run}).Discard,hrf);
+    Static_effect = conv(vols.(runs{run}).Static,hrf);
+    MAE_effect = conv(vols.(runs{run}).MAE,hrf);
     
-    task_effect.CompPatt.(runs{run}) = CompPatt_effect;
-    task_effect.PattComp.(runs{run}) = PattComp_effect;
-    task_effect.Discard.(runs{run}) = Discard_effect;
-    task_effect.Static.(runs{run}) = Static_effect;
-    task_effect.MAE.(runs{run}) = MAE_effect;
+    task_effect.CompPatt.(runs{run}) = CompPatt_effect(1:266,1);
+    task_effect.PattComp.(runs{run}) = PattComp_effect(1:266,1);
+    task_effect.Discard.(runs{run}) = Discard_effect(1:266,1);
+    task_effect.Static.(runs{run}) = Static_effect(1:266,1);
+    task_effect.MAE.(runs{run}) = MAE_effect(1:266,1);
 end
 
 %% path to the ROI timeseries and to the motion parameters
@@ -107,6 +113,14 @@ end
 %plot(1:266, roi.y(:,1), 'r', 1:266, roi.y(:,2), 'b', 1:266, r1, 'g', 1:266, r2, 'k')
 % plot( 1:266, roi.y(:,2), 'b', 1:266, r2 +mean(roi.y(:,2)), 'k')
 % legend('hMT right timeseries','hMT2 right residuals')
-
-
+% hold on
+% plot(1:266, roi.y(:,2), 'b')
+% plot(1:266, r2 +mean(roi.y(:,2)), 'k')
+% plot(1:266, 154 +task_effect.CompPatt.(runs{run}), 'r')
+% plot(1:266, 154 +task_effect.PattComp.(runs{run}), 'g')
+% plot(1:266, 154 +task_effect.Discard.(runs{run}), 'c')
+% plot(1:266, 154 +task_effect.Static.(runs{run}), 'm')
+% plot(1:266, 154 +task_effect.MAE.(runs{run}), 'color', [0.4940 0.1840 0.5560])
+% 
+% legend('hMT', 'denoised hMT', 'Effect CompPatt', 'Effect PattComp', 'Effect Discard', 'Effect Static', 'Effect MAE')
 
